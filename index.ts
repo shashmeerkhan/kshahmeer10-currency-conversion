af09d2e170f5556bcae097c8c3e2b7583b67ff6e
Shahmeer Khan <kshahmeer10@gmail.com>
2024-04-13 09:25:45 +0500

    kshahmeer10-currency-conversion

---
+++ index.ts
@@ -0,0 +1,56 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+//currency_conversion object
+let currency_conversion = {
+  PKR: {
+    USD: 0.0036,
+    GBP: 0.0028,
+    PKR: 1,
+  },
+
+  GBP: {
+   USD: 1.26,
+    GBP: 1,
+   PKR: 350.59,
+  },
+  USD: {
+    USD: 1,
+    GBP: 0.79,
+    PKR: 277.54,
+  },
+};
+//prompt user for input
+
+const answer: {
+    from: "PKR" | "GBP" | "USD",
+    to: "PKR" | "GBP" | "USD",
+    amount: number,
+  } = await inquirer.prompt([{
+    type: "list",
+    name: "from",
+    message: "please select your currency",
+    choices: ["PKR", "GBP", "USD"],
+  },
+  {
+    type: "list",
+    name: "to",
+    message: "select your conversion rate",
+    choices: ["PKR", "GBP", "USD"],
+  },{
+    type: "number",
+    name: "amount",
+    message: "please enter the amount you want to convert",
+  }
+]);
+//destructuring  user input
+
+const { from, to, amount } = answer;
+
+//calculate the conversion rate
+if ( from && to && amount){let result=currency_conversion[from][to] * amount
+    console.log(`your conversion rate ${from} to ${to} is \n ${result}`);
+    
+}
+else {console.log("invalid conversion" );
+}
